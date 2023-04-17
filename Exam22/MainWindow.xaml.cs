f206e19abdcd62c26824da4534e1a5bc945145d4
Witcher <haritonovhuk@inbox.ru>
2023-04-17 12:15:06 +0700

    Natk

---
+++ Exam22/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam22
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBox1.Text.Length < 4 || textBox1.Text.Length > 50) 
+            {
+                textBox1.Background = Brushes.Red;
+                string text = "Минимальное количество символов 4, максимальное 50";
+                textBloc1.Text = text;
+
+            }
+            else
+            {
+                textBox1.Background = Brushes.Yellow;
+                string text1 = "Всё верно";
+                textBloc1.Text = text1;
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBox2.Text.Length < 10 || textBox2.Text.Length > 35) 
+            {
+                textBox2.Background = Brushes.Red;
+                string text = "Минимальное количество символов 10, максимальное 35";
+                textBloc2.Text = text;
+
+            }
+            else
+            {
+                textBox2.Background = Brushes.Yellow;
+                string text1 = "Всё верно";
+                textBloc2.Text = text1;
+            }
+
+        }
+
+        private void textBoxminus_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, e.Text.Length - 1) || (textBoxminus.Text == "" && e.Text == "-"))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Name_4_ReturnsFalse() //меньше 4
+        {
+            string name = "ab";
+            bool result = IsNameValid(name);
+            Assert.IsFalse(result);
+        }
+
+
+        public void Name_50_ReturnsFalse() //больше 50
+        {
+            string name = "abcdefghijklmnop";
+            bool result = IsNameValid(name);
+            Assert.IsFalse(result);
+        }
+
+
+        public void Name_4And50_ReturnsTrue() // от 4 до 50
+        {
+            string name1 = "abc";
+            string name2 = "abcdefghijklmno";
+
+            bool result1 = IsNameValid(name1);
+            bool result2 = IsNameValid(name2);
+
+            Assert.IsTrue(result1);
+            Assert.IsTrue(result2);
+        }
+
+        private bool IsNameValid(string name)
+        {
+            if (name.Length < 3 || name.Length > 15) // Проверяем, что длина имени находится в диапазоне от 3 до 15 символов
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+
+        [TestMethod]
+        public void Name_WithLessThan10_ReturnsFalse() //меньше 10
+        {
+            string name = "ab";
+            bool result = IsNameValid2(name);
+            Assert.IsFalse(result);
+        }
+
+
+        public void Name_WithMoreThan35_ReturnsFalse() //больше 35
+        {
+            string name = "abcdefghijklmnop";
+            bool result = IsNameValid2(name);
+            Assert.IsFalse(result);
+        }
+
+
+        public void Name_10And35_ReturnsTrue() // от 10 до 35
+        {
+            string name1 = "abc";
+            string name2 = "abcdefghijklmno";
+
+            bool result1 = IsNameValid2(name1);
+            bool result2 = IsNameValid2(name2);
+
+            Assert.IsTrue(result1);
+            Assert.IsTrue(result2);
+        }
+
+        private bool IsNameValid2(string name)
+        {
+            if (name.Length < 10 || name.Length > 35) // Проверяем, что длина имени находится в диапазоне от 3 до 15 символов
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
